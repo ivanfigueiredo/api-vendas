fc6e561134317565b57d36bfdd3e148214cb8a68
ivanfigueiredo <ivanfigueiredo.wm@gmail.com>
2022-06-23 11:49:20 -0300

    feat: injected dependency

--- src/cell.module.ts
+++ src/cell.module.ts
@@ -1,3 +1,6 @@
+import { LoadCellUseCase } from './domain/use-case/load-cell/load-cell.use-case'
+import { LoadCellController } from './presentation/load-cell/load-cell.controller'
+import { LoadCellByCodePersister } from './infra/db-mongo/load-cell/load-cell.persistir'
 import { AddCellUseCase } from './domain/use-case/add-cell.use-case/add-cell.use-case'
 import { CellMapper } from './infra/mapper/cell.mapper'
 import { SaveCellPersister } from './infra/db-mongo/add-cell/save-cell.persister'
@@ -10,4 +13,10 @@ const addCellUseCase = new AddCellUseCase(saveCellPersister)
 
 const addCellController = new AddCellController(addCellUseCase)
 
-export { addCellUseCase, addCellController }
+const loadCellByCode = new LoadCellByCodePersister()
+
+const loadCellUseCase = new LoadCellUseCase(loadCellByCode)
+
+const loadCellController = new LoadCellController(loadCellUseCase)
+
+export { addCellUseCase, loadCellUseCase, loadCellController, addCellController }
