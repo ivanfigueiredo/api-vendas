74f874dfa4ca59d8883aee1ead2cea23f499bafb
ivanfigueiredo <ivanfigueiredo.wm@gmail.com>
2022-06-23 14:42:50 -0300

    feat: create router group-cells

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express'
-import { addCellController, loadCellController, updateCellController } from '../../cell.module'
+import {
+  addCellController,
+  loadCellController,
+  updateCellController,
+  groupCellsController
+} from '../../cell.module'
 const router = Router()
 
 router.post('/add', async (req, res) => {
   const result = await addCellController.handle(req)
-
   return res.json({ result }).send()
 })
 
@@ -15,6 +19,11 @@ router.get('/cellCode', async (req, res) => {
 
 router.put('/updateOne', async (req, res) => {
   const result = await updateCellController.handle(req)
+  return res.json({ result }).send()
+})
+
+router.get('/groupcells', async (req, res) => {
+  const result = await groupCellsController.handle(req)
   return res.json({ result }).send()
 })
 
