cc1891e98d276dddad109f8847c6d8d63a430e99
ivanfigueiredo <ivanfigueiredo.wm@gmail.com>
2022-06-23 11:52:41 -0300

    feat: controller to uptadeOne

---
+++ src/presentation/update-cell/update-cell.controller.ts
@@ -0,0 +1,21 @@
+import { InvalidParamError } from './../../helpers/errors/invalid-param-error'
+import { badRequest, Success } from './../../helpers/http-helper'
+import { HttpRequest, HttpResponse } from '../../helpers/http'
+import { Controller } from './../controller/controller'
+import { LoadCellUseCase } from '../../domain/use-case/load-cell/load-cell.use-case'
+
+export class UpdateCellController implements Controller {
+  constructor (
+    private readonly loadCellUseCase: LoadCellUseCase
+  ) {}
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    const cell = await this.loadCellUseCase.execute(httpRequest.body)
+
+    if (!cell) {
+      return badRequest(new InvalidParamError('CODE_NOT_FOUND'))
+    }
+
+    return Success(cell)
+  }
+}
