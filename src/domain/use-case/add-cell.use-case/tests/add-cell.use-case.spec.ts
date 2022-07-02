fa13c3449007057dbfca16dd0971a1833f748bfb
ivanfigueiredo <ivanfigueiredo.wm@gmail.com>
2022-07-02 16:37:46 -0300

    test: should be able to call save with correct values

--- src/domain/use-case/add-cell.use-case/tests/add-cell.use-case.spec.ts
+++ src/domain/use-case/add-cell.use-case/tests/add-cell.use-case.spec.ts
@@ -1,22 +1,15 @@
 import { MockAddCellRepository } from '../../../../infra/db-mongo/mock/add-cell.mock'
 import { IAddCellRepo } from '../../../../repo/add-cell.repo'
 import { AddCellUseCase } from '../add-cell.use-case'
-import { Color } from '../add-cell.use-case.dto'
+import { AddCellUseCaseDto, Color } from '../add-cell.use-case.dto'
 
 describe('add-cell.use-case', () => {
   let useCase: AddCellUseCase
   let addCellRepo: IAddCellRepo
+  let dto: AddCellUseCaseDto
 
   beforeAll(() => {
-    addCellRepo = MockAddCellRepository()
-    useCase = new AddCellUseCase(addCellRepo)
-  })
-  it('should br defined', async () => {
-    expect(useCase).toBeDefined()
-  })
-
-  it('should add cell with success', async () => {
-    const dto = {
+    dto = {
       code: '11012025',
       model: 'AHFDJHERERH',
       brand: 'brand',
@@ -25,7 +18,22 @@ describe('add-cell.use-case', () => {
       price: 333,
       color: 'BLACK' as Color
     }
+    addCellRepo = MockAddCellRepository()
+    useCase = new AddCellUseCase(addCellRepo)
+  })
+  it('should br defined', async () => {
+    expect(useCase).toBeDefined()
+  })
+
+  it('should add cell with success', async () => {
     const result = await useCase.execute(dto)
     expect(result.body).toBeTruthy()
+  })
+
+  it('should be able to call save with correct values', async () => {
+    const spySave = jest.spyOn(addCellRepo, 'save')
+    const result = await useCase.execute(dto)
+    expect(result.body).toBeTruthy()
+    expect(spySave).toHaveBeenCalled()
   })
 })
